09107814b4cfc487737d8ff1b55249358333c892
justin <144488087+yaoolee@users.noreply.github.com>
2025-10-19 23:48:02 -0400

    create footer component and change width of login button, subscribe and sign up button

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 11V7C8 5.67392 8.52678 4.40215 9.46447 3.46447C10.4021 2.52678 11.6739 2 13 2C14.3261 2 15.5979 2.52678 16.5355 3.46447C17.4732 4.40215 18 5.67392 18 7V11" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/><path d="M19 12H7C5.89543 12 5 12.8954 5 14V20C5 21.1046 5.89543 22 7 22H19C20.1046 22 21 21.1046 21 20V14C21 12.8954 20.1046 12 19 12Z" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/></svg>
             Login
             </a>
-            <Button>Sign up</Button>
+            <Button className="signup-button">Sign up</Button>
           </div>
         </div>
       </div>
--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,16 @@
 import Header from '../../components/Header/Header.jsx';
+import Footer from '../../components/Footer/Footer';
 import React from 'react';
 
 
 const LandingPage = () => {
   return (
+    <div>
    <main>
     <Header />
     </main>
+    <Footer />
+    </div>
   );
 };
 

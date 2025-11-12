5cbae1f2ee21373a13f81ea131f877896989aa78
justin <144488087+yaoolee@users.noreply.github.com>
2025-11-11 19:47:53 -0500

    remove reliance on dev proxy for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5050",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-});
+  server: mode === "development"
+    ? {
+        proxy: {
+          "/api": {
+            target: "http://localhost:5050",
+            changeOrigin: true,
+            secure: false,
+          },
+        },
+      }
+    : undefined,
+}));
